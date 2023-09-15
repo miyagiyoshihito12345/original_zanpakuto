746afe9d3de346811f69cee7715d4b8d03c2b8f6
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-09-15 12:32:19 +0900

    Add: フラッシュメッセージ、バリデーションエラーの個別表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to root_path
+      redirect_to root_path, success: t('.success')
     else
-      render :new
+      flash.now[:danger] = t('.fail')
+      render :new, status: :unprocessable_entity
     end
   end
 
