0f9c6bfb058c70c5488d3e8354d0c931c271873f
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-09-16 15:42:45 +0900

    Add: プロフィール編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,16 +11,14 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to profiles_path, success: t('defaults.message.created', item: Post.model_name.human)
+      redirect_to '/#post', success: t('defaults.message.created', item: Post.model_name.human)
     else
       flash.now['danger'] = t('defaults.message.not_created', item: Post.model_name.human)
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   private
 
