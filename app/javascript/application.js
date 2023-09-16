2acd07f80fac440dfab2b217ab6f8248f1b82249
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-09-16 12:50:59 +0900

    Fix: ファイル修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,5 +5,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./custom/preview.js"
 
