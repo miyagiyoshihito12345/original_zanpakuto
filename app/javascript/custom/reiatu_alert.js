94421fdb2c104d14771815febedaf7188b73d5ae
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-10-02 12:50:44 +0900

    Fix: デプロイのバグ修正

--- app/javascript/custom/reiatu_alert.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-	const buttons = document.querySelectorAll('.button');
-
-	buttons.forEach(button => {
-		button.addEventListener('click', () => {
-			alert("ログイン後にいいね機能が使えます");          
-		});    
-	}); 
