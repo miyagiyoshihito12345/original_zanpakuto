7be56b14d3336100000ca8fd493827726774f982
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-10-26 22:34:45 +0900

    Fix: sessionの有効期限の設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_count_session', expire_after: 6.hour
-
+Rails.application.config.session_store :cookie_store, key: '_count_session', expire_after: 8.hours
