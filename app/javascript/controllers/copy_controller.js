016109eaaa2b15d5e8dfe6ef23c1dbdc1bb1e5c8
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-10-29 17:51:02 +0900

    Fix: Clipboard APIでコピーする

--- app/javascript/controllers/copy_controller.js
+++ app/javascript/controllers/copy_controller.js
@@ -3,10 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 	static targets = ["text"]
 	copy_text() {
-		//this.loadingTarget.classList.remove("disvisible");
-		//this.loadingTarget.classList.add("modal_ai");
-		this.textTarget.select();
-		document.execCommand("Copy");
-		alert("コピーしました。")
+		navigator.clipboard.writeText(this.textTarget.value).then(
+			() => {
+				alert("コピーしました。")
+			},
+			() => {
+				alert("クリップボードにコピー出来ませんでした。")
+			}
+		)
 	}   
 }
